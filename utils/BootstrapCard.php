2328d24807c6e9eef567031f0131e38667eac4ea
davgui35 <dg1976317@gmail.com>
2021-02-22 19:25:07 +0100

    project initial

---
+++ utils/BootstrapCard.php
@@ -0,0 +1,17 @@
+<?php
+class BootstrapCard
+{
+    // :TODO => faire la carte par défaut
+    //- checkbox / la description/title / favoris / 
+    //:TODO => Faire la validation des données de la carte
+    /**<div class="card">
+     <div class="card-header">
+        Featured
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div> */
+}
